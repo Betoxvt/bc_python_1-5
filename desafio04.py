626ac6f93295638aa1bb477e37d597c7bdbe51db
Roberto Guedes <robertoguedes@edu.univali.br>
2024-09-02 20:53:17 -0300

    desafio 4

--- desafio04.py
+++ desafio04.py
@@ -1,71 +1,69 @@
 # Escreva um programa em Python que solicita ao usuário que digite seu nome, o valor do seu salário mensal e o valor do bônus que recebeu, O programa deve, então , imprimir uma mensagem saudando o usuário pelo nome e informando o valor do salário em comparação com o bônus recebido. + Com o tratamento de errors. + Loop de tentativas. + type hint, função, dicionários.
 
-# Constante do KPI 2024
-CONSTANTE_BONUS = 1000
 
-# Iniciando variáveis booleanas
-nome_valido = False
-salario_valido = False
-bonus_valido = False
+def linha() -> str:
+    print('-'*50)
 
-try:
-    # 1) Solicita ao usuário que digite seu nome
-    while not nome_valido:
+
+def titulo(mensagem=' ') -> str:
+    linha()
+    print(mensagem.center(50))
+    linha()
+
+
+def inserir_nome_valido() -> str:
+    while True:
         try:
-            nome = str(input("Insira seu nome: ").strip().title())
+            nome: str = str(input("\nInsira seu nome: ").strip().title())
             if not nome.replace("'", "").replace(' ', '').isalpha():
                 raise ValueError('O nome contém caractéres inválidos.')
         except ValueError as e:
-            print(f'ERRO. {e}')
+            print(f'\n\033[31mERRO. {e}.\033[m')
         else:
-            print(f'Nome válido registrado: {nome}')
-            nome_valido = True
+            print(f'\n\033[32mNome válido\033[m')
+            return nome
+        
 
+def inserir_numero_float_valido(mensagem='ponto flutuante') -> float:
+    while True:
+        try:
+            numero: float = float(input(f'\nInsira o valor para {mensagem}: ').strip().replace(',', '.'))
+            if numero < 0:
+                raise ValueError
+        except ValueError:
+            print(f'\n\033[31mERRO. O valor deve ser um número positivo.\033[m')
+        else:
+            print(f'\n\033[32mValor de {mensagem} válido\033[m')
+            return numero
+        
+
+def calcular_kpi(salario='0', bonus='0') -> float:
+    CONSTANTE_BONUS: int = 1000
+    kpi: float = CONSTANTE_BONUS + (bonus * salario)
+    return kpi
+
+
+# Programa principal
+try:
+    titulo('Calculadora KPI 2024')
+    # 1) Solicita ao usuário que digite seu nome
+    nome: str = inserir_nome_valido()
     # 2) Solicita ao usuário que digite o valor do seu salário
     # Converte a entrada para um número de ponto flutuante
-    while not salario_valido:
-        try:
-            salario = float(input("Insira o valor de seu salário mensal: R$").strip().replace(',', '.'))
-            if salario < 0:
-                raise ValueError('O salário deve ser um número positivo')
-        except ValueError as e:
-            print(f'Valor inválido. {e}')
-        except Exception as e:
-            print(f'Erro, {e}')
-        else:
-            print(f'Salário registrado: R${salario:.2f}')
-            salario_valido = True
-
+    salario: float = inserir_numero_float_valido('salário')
     # 3) Solicita ao usuário que digite o valor do bônus recebido
     # Converte a entrada para um número de ponto flutuante
-    while not bonus_valido:
-        try:
-            bonus = float(input("Insira o bônus (%): ")) / 100
-            if bonus < 0:
-                raise ValueError('O bônus deve ser um número positivo')
-        except ValueError as e:
-            print(f'Valor inválido. {e}')
-        except Exception as e:
-            print(f'Erro, {e}')
-        else:
-            print(f'Bônus de {bonus*100}% = {bonus}')
-            bonus_valido = True
+    bonus: float = inserir_numero_float_valido('bônus')   
 except KeyboardInterrupt:
-    print('\nSistema terminado pelo usuário.')
+    print('\n\033[31mSistema terminado pelo usuário.\033[m')
 else:
-
     # 4) Calcule o valor do bônus final
-    bonus_final = bonus * salario
-
+    kpi: float = calcular_kpi(salario, bonus)
     # 5) Imprima cálculo do KPI para o usuário
-    kpi = CONSTANTE_BONUS + bonus_final
-    print(f'Cálculo do KPI:\nKPI = 1000 + ({salario} X {bonus}) = R${kpi}')
-
+    print(f'\nKPI = 1000 + ({salario} X {bonus}) = {kpi}')
     # 6) Imprime a mensagem personalizada incluindo o nome do usuário, salário e bônus
-    novo_salario = salario + kpi
-    print(f'Saudações {nome}, seu salário agora é de R${novo_salario:.2f}')
+    print(f'\n\033[34mSaudações {nome}, seu salário é R$ {salario:.2f} e seu bônus R$ {kpi:.2f}\033[m\n')
 finally:
-    print('Fim do programa')
-
+    titulo('Fim do programa')
 # Bônus: Quantos bugs e riscos você consegue identificar nesse programa?
 # Problemas em erros de digitação, como usar vírgula ao invés de ponto para os números com casas decimais ou trocar a ordem das entradas.
